3afd11ff60148f474fb65e12d7192abe6a01d93a
Amey2303 <ameyasrane@gmail.com>
2023-10-18 15:07:10 +0530

    My first react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+function Hello() {
+  function toggleGreet() {
+    var greetText = document.querySelector('#heading');
+    var greetLabel = document.querySelector('#btnLabel');
 
-function App() {
+    if (greetText.textContent === "Hello World!") {
+      greetText.textContent = "Hello Amey! :)";
+      greetLabel.textContent = "Greet World";
+    } else {
+      greetText.textContent = "Hello World!";
+      greetLabel.textContent = "Greet User!";
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 id="heading">Hello World!</h1>
+      <button id="btnLabel" onClick={toggleGreet}>
+        Greet User!
+      </button>
     </div>
   );
 }
 
-export default App;
+export default Hello;
